0ab111b1be798f0e29e936755f99763553fdb230
kavyasri kota <Kavyasrikota989@gmail.com>
2026-07-13 21:54:41 +0530

    Update DSA.py

--- DSA.py
+++ DSA.py
@@ -1,28 +1,28 @@
 #write a code to separete digits of a number
-'''n=int(input("Enter a number:"))
+n=int(input("Enter a number:"))
 while n!=0:
     digit=n%10
     print(digit)
-    n//=10'''
+    n//=10
   
 
 #write a code tomprint the symof digits of a number
-'''n=int(input("Enter a number:"))
+n=int(input("Enter a number:"))
 sum=0
 while n!=0:
     digit=n%10
     sum+=digit
     n//=10
-print(sum)'''
+print(sum)
 
 #write a code to print sum of series
-'''n=int(input("Enetr a number:"))
+n=int(input("Enetr a number:"))
 sum=0
 i=1
 while i<=n:
     sum+=i/(i+1)
     i+=1
-print(sum)'''
+print(sum)
 
 
 
