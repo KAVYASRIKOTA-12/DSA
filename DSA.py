c80bca8a2bfa67dc0bd4a44c5228c6639672f2f7
kavyasri kota <Kavyasrikota989@gmail.com>
2026-07-13 21:53:37 +0530

    Add files via upload

---
+++ DSA.py
@@ -0,0 +1,28 @@
+#write a code to separete digits of a number
+'''n=int(input("Enter a number:"))
+while n!=0:
+    digit=n%10
+    print(digit)
+    n//=10'''
+  
+
+#write a code tomprint the symof digits of a number
+'''n=int(input("Enter a number:"))
+sum=0
+while n!=0:
+    digit=n%10
+    sum+=digit
+    n//=10
+print(sum)'''
+
+#write a code to print sum of series
+'''n=int(input("Enetr a number:"))
+sum=0
+i=1
+while i<=n:
+    sum+=i/(i+1)
+    i+=1
+print(sum)'''
+
+
+
