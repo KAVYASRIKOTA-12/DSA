7e163a583b73774da159339f981f51e5a6dc94df
kavyasri kota <Kavyasrikota989@gmail.com>
2026-07-14 17:25:51 +0530

    Update #program to print a square pattern.py

--- #program to print a square pattern.py
+++ #program to print a square pattern.py
@@ -1,101 +1,105 @@
 #program to print a square pattern
-'''n=int(input("Enter the size: "))
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         print("*",end=" ")
-    print()'''  
+    print()
 
 #To print hallow square
-'''n=int(input("Enter the size: "))
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         if i==0 or i==n-1 or j==0 or j==n-1:
             print("*",end=' ')
         else:
             print(" ",end=' ')    
-    print()'''
+    print()
 
 
-'''n=int(input("Enter the size: "))
+#another pattern
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         if i==0 or i==n-1 or j==0 or j==n-1 or i==j or i+j==n-1:
             print("*",end=' ')
         else:
             print(" ",end=' ')    
-    print()'''
+    print()
         
 
-'''n=int(input("Enter the size: "))
+#To print hourglass
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         if i==0 or i==n-1 or i==j or i+j==n-1:
             print("*",end=' ')
         else:
             print(" ",end=' ')    
-    print()'''
+    print()
 
 
-'''n=int(input("Enter the size: "))
+#To print butterfly
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         if  j==0 or j==n-1 or i==j or i+j==n-1:
             print("*",end=' ')
         else:
             print(" ",end=' ')    
-    print()'''
+    print()
 
 
-'''n=int(input("Enter the size: "))
+#to print cross(X)
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         if  i==j or i+j==n-1:
             print("*",end=' ')
         else:
             print(" ",end=' ')    
-    print()'''
+    print()
 
-
-'''n=int(input("Enter the size: "))
+#to print plus symbol(+)
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         if i==n//2 or j==n//2:
             print("*",end=' ')
         else:
             print(" ",end=' ')    
-    print()'''
+    print()
 
-
-'''n=int(input("Enter the size: "))
+#to print right angle triangle
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         if j==0 or i==n-1 or i==j:
             print("*",end=' ')
         else:
             print(" ",end=' ')    
-    print()'''
+    print()
 
-
-'''n=int(input("Enter the size: "))
+#to print mirror right-angle triangle
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         if j==n-1 or i==n-1 or i+j==n-1:
             print("*",end=' ')
         else:
             print(" ",end=' ')    
-    print()'''
+    print()
 
-
-'''n=int(input("Enter the size: "))
+#to print inverted right-angle triangle
+n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(n):
         if i==0 or j==0 or i+j==n-1:
             print("*",end=' ')
         else:
             print(" ",end=' ')    
-    print()'''
+    print()
 
-# to print right-angle triangle
+# to print full right-angle triangle
 '''n=int(input("Enter the size: "))
 for i in range(n):
     for j in range(i):
@@ -103,14 +107,14 @@ for i in range(n):
     print()'''
 
 
-# to print inverted rightangled triangle
+# to print full inverted rightangled triangle
 '''n=int(input("Enter the size: "))
 for i in range(n,0,-1):
     for j in range(i):
         print("*",end=' ')   
     print()'''
 
-# to print symmetric 
+# to print full symmetric right-angled triangle
 n=int(input("Enter the size: "))
 for i in range(1,n+1):
     for j in range(n-i):
