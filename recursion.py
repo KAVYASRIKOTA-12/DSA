8cbcbf554433260e5addc7489b2e69b4eaac91ab
kavyasri kota <Kavyasrikota989@gmail.com>
2026-07-16 20:00:29 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -1,51 +1,51 @@
 #arg pass - return value
 #TC-O(1),SC-O(1)
-'''def summate(a,b):
+def summate(a,b):
     return a+b
 num1=int(input("Enter value:"))
 num2=int(input("Enter value:"))
 result= summate(num1,num2)
-print("Sum:",result)'''
+print("Sum:",result)
 
 #arg pass - no return value
 #TC-O(1),SC-O(1)
-'''def summate(a,b):
+def summate(a,b):
     print("Sum:",a+b)
 num1=int(input("Enter value:"))
 num2=int(input("Enter value:"))
-summate(num1,num2)'''
+summate(num1,num2)
 
 
 #no arg - return value
 #TC-O(1),SC-O(1)
-'''def summate():
+def summate():
     num1=int(input("Enter value:"))
     num2=int(input("Enter value:"))
     return num1+num2
-print("Sum:",summate())'''
+print("Sum:",summate())
 
 
 #no arg - no return value
 #TC-O(1),SC-O(1)
-'''def summate():
+def summate():
     num1=int(input("Enter value:"))
     num2=int(input("Enter value:"))
     print("Sum:",num1+num2)
-summate()'''
+summate()
 
 
 #direct recursion
 #TC-O(n),SC-O(n)
-'''def fact(n):
+def fact(n):
     if n==1:
         return 1
     return n*fact(n-1)
 n=int(input("Enter a value:"))
-print(fact(n))'''
+print(fact(n))
 
 #indirect recursion-checking even or odd without %
 #TC-O(n+1),SC-O(n)
-'''def even(n):
+def even(n):
     if n==0:
         return True
     return odd(n-1)
@@ -54,7 +54,7 @@ def odd(n):
         return False
     return even(n-1)
 n=int(input("Enter a value:"))
-print(even(n))'''
+print(even(n))
 
 #tail recursion
 def num(n):
