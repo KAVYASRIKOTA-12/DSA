845ad44f762e08dfb616d57a7b69de86bf74940c
kavyasri kota <Kavyasrikota989@gmail.com>
2026-07-27 16:02:17 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -31,5 +31,28 @@ for i in range(1,n):
     arr[j+1]=x    
 print(*arr)
 
-
+#merge sort
+#divide and conquer
+#TC-O(nlogn),SC-O(n)
+def merge_sort(arr):
+    if len(arr)<=1:
+        return arr
+    mid=len(arr)//2
+    left=merge_sort(arr[:mid])
+    right=merge_sort(arr[mid:])
+    result=[]
+    i=j=0
+    while i<len(left) and j<len(right):
+        if left[i]<right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result            
+arr=list(map(int,input("Enter values:").split()))
+sorted_arr=merge_sort(arr)
+print(*sorted_arr)
 
